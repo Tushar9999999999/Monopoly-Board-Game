88fd134068aeea41cd66f9a1084fa1edcc3299c0
Tushar <76560857+Tushar9999999999@users.noreply.github.com>
2021-08-19 16:15:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,21 +10,11 @@ int main()
     while(TRUE)
     {
         clearScreen();
-        printf("\n                                              888b     d888  .d88888b.  888b    888  .d88888b.  8888888b.   .d88888b.  888    Y88b   d88P           "); 
-        printf("\n                                              8888b   d8888 d88P\" \"Y88b 8888b   888 d88P\" \"Y88b 888   Y88b d88P\" \"Y88b 888     Y88b d88P      ");       
-        printf("\n                                              88888b.d88888 888     888 88888b  888 888     888 888    888 888     888 888      Y88o88P             "); 
-        printf("\n                                              888Y88888P888 888     888 888Y88b 888 888     888 888   d88P 888     888 888       Y888P              "); 
-        printf("\n                                              888 Y888P 888 888     888 888 Y88b888 888     888 8888888P\"  888     888 888        888              ");  
-        printf("\n                                              888  Y8P  888 888     888 888  Y88888 888     888 888        888     888 888        888               "); 
-        printf("\n                                              888   \"   888 Y88b. .d88P 888   Y8888 Y88b. .d88P 888        Y88b. .d88P 888        888              ");  
-        printf("\n                                              888       888  \"Y88888P\"  888    Y888  \"Y88888P\"  888         \"Y88888P\"  88888888   888         ");       
-        printf("\n\n\n\n\n");
+        printf("MONOPOLY\n\n\n");
         printf("\n\t  1-Play Monopoly");
         printf("\n\t  2-Read Rules");
         printf("\n\t  X-Exit\n\n");
-        hideCursor();
         choice = getch();
-        showCursor();
         if (choice == '1')
         {
             if (mainGame()!=EXIT_SUCCESS)
